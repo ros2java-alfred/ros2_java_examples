91bf33ab8dbdc4ec6b75c18deffb61a69f1b61d4
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2017-08-09 01:00:13 +0200

    Add Lambda examples

---
+++ rcljava_examples/src/main/java/org/ros2/rcljava/examples/minimal/publisher/Lambda.java
@@ -0,0 +1,66 @@
+/* Copyright 2017 Mickael Gaillard <mick.gaillard@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ros2.rcljava.examples.minimal.publisher;
+
+import java.util.concurrent.TimeUnit;
+
+import org.ros2.rcljava.RCLJava;
+import org.ros2.rcljava.node.NativeNode;
+import org.ros2.rcljava.node.topic.Publisher;
+import org.ros2.rcljava.time.WallTimer;
+
+public class Lambda {
+
+    public static class MinimalPublisher extends NativeNode {
+        int i = 0;
+        private Publisher<std_msgs.msg.String> pub;
+        private WallTimer timer;
+
+        public MinimalPublisher() {
+            super("minimal_subscriber");
+
+            this.pub = this.<std_msgs.msg.String>createPublisher(
+                            std_msgs.msg.String.class,
+                            "topic");
+
+            this.timer = this.createWallTimer(500, TimeUnit.MILLISECONDS, () -> tick() );
+            this.timer.getHandle(); // Disable Warning
+        }
+
+        public void tick() {
+            std_msgs.msg.String msg = new std_msgs.msg.String();
+            msg.setData("Hello World: " + ++i);
+
+            System.out.println("Publishing: \"" + msg.getData() + "\"");
+            this.pub.publish(msg);
+        }
+
+        @Override
+        public void dispose() {
+            this.timer.dispose();
+            this.pub.dispose();
+            super.dispose();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        // Initialize RCL
+        RCLJava.rclJavaInit(args);
+
+        RCLJava.spin(new MinimalPublisher());
+
+        RCLJava.shutdown();
+    }
+}
---
+++ rcljava_examples/src/main/java/org/ros2/rcljava/examples/minimal/subscriber/Lambda.java
@@ -0,0 +1,53 @@
+/* Copyright 2017 Mickael Gaillard <mick.gaillard@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ros2.rcljava.examples.minimal.subscriber;
+
+import org.ros2.rcljava.RCLJava;
+import org.ros2.rcljava.node.NativeNode;
+import org.ros2.rcljava.node.topic.Subscription;
+
+public class Lambda {
+
+    public static class MinimalSubscriber extends NativeNode {
+        private Subscription<std_msgs.msg.String> sub;
+
+        public MinimalSubscriber() {
+            super("minimal_subscriber");
+
+            // Subscriptions are type safe, so we'll pass the message type. We use the fully
+            // qualified
+            // class name to avoid any collision with Java's String class
+            this.sub = this.<std_msgs.msg.String>createSubscription(
+                    std_msgs.msg.String.class,
+                    "topic",
+                    (std_msgs.msg.String msg) -> { System.out.println("I heard: [" + msg.getData() + "]"); } );
+        }
+
+        @Override
+        public void dispose() {
+            this.sub.dispose();
+            super.dispose();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        // Initialize RCL
+        RCLJava.rclJavaInit(args);
+
+        RCLJava.spin(new MinimalSubscriber());
+
+        RCLJava.shutdown();
+    }
+}
